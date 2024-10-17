c1191862d407a277f2cb8df122871f920910954d
김승혜 <67942699+kshksh723@users.noreply.github.com>
2024-10-17 15:35:42 +0900

    Title: 세 정수형 변수 선언, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241017/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+      int a,b,c;
+      a = 7;
+      b = 23;
+      c = a + b;
+      System.out.printf(a + " + " + b+" = "+ c);
+    }
+}
