019cce42ec63019376943af46f23945435d8c2cd
김승혜 <67942699+kshksh723@users.noreply.github.com>
2024-10-18 14:37:07 +0900

    Title: 길이 단위 변환하기, Time: 94ms, Memory: 9MB, Status: Passed - CodeTree

--- 241018/길이 단위 변환하기/change-length-unit.java
+++ 241018/길이 단위 변환하기/change-length-unit.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        double ft = 30.48;
-        double mi = 160934;
+        //1피트(ft)는 30.48cm이고 1마일(mi)은 160934cm입니다. 
+        // 변수 선언
+        double a = 9.2; //9.2피트
+	    double b = 1.3; // 1.3마일
 
-        System.out.printf("%.1f", ft,mi);
+        // 출력
+        System.out.printf("%.1fft = %.1fcm\n%.1fmi = %.1fcm", a, a*30.48, b, b*160934); // 피트 수에 제시된 값 곱하기
     }
 }
+// %.1fft = %.1fcm은 피트(feet)를 센티미터(cm)로 변환하는 공식 
+
+// %.1fmi = %.1fcm은 마일(mile)을 센티미터(cm)로 변환하는 공식
